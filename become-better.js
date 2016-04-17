df82b5ee1dc8d610880e2613caad5de1d3de1324
Dkra <dkra4741@gmail.com>
2016-04-17 23:31:05 +0800

    module pattern example-1

--- become-better.js
+++ become-better.js
@@ -1,23 +1,46 @@
-function delayAndSay (time, msg) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function () {
-      // value pass in to resolve is "Value return in feature"
-      resolve(msg);
-    }, time);
-  });
-}
+// The Module Pattern
 
-delayAndSay(1000,'hey')
-  .then(function(data) {
-    console.log(data);
-    return delayAndSay(2000,'man');
-  },function(data) {
-    console.log('resovle'+data);
-  })
-  .then(function(data) {
-    console.log(data);
-    return delayAndSay(2000,'yo');
-  })
-  .then(function(data) {
-    console.log(data);
-  });
+// only a public API is returned, keeping everything else within the closure private.
+
+
+// Example 1
+
+var basket = (function() {
+  // private varialbe
+  var list = [],
+      total = 0;
+
+  // public methods
+  return {
+    addItem: function(item) {
+      list.push(item);
+    },
+    totalPrice: function() {
+      list.forEach(function(obj){
+        console.log(obj);
+        total = total + obj.price * obj.amount;
+      });
+      return total;
+    },
+    showList: function() {
+      return list;
+    }
+  };
+
+})();
+
+
+basket.addItem({
+  name: 'apple',
+  amount: 1,
+  price: 10
+});
+
+basket.addItem({
+  name: 'kiwi',
+  amount: 3,
+  price: 20
+});
+
+console.log(basket.showList());
+console.log(basket.totalPrice());
