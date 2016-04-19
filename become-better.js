30c58f77cef99e65c1b35fb6f98f4640c33e5c36
Dkra <dkra4741@gmail.com>
2016-04-19 19:25:04 +0800

    revealing module pattern implementation

--- become-better.js
+++ become-better.js
@@ -1,46 +1,56 @@
-// The Module Pattern
+// Revealing Module Pattern
 
-// only a public API is returned, keeping everything else within the closure private.
+/*
+
+- Advantages
+
+This pattern allows the syntax of our scripts to be more consistent. It also makes it more clear at the end of the module which of our functions and variables may be accessed publicly which eases readability.
+
+- Disadvantages
+
+A disadvantage of this pattern is that if a private function refers to a public function, that public function can't be overridden if a patch is necessary. This is because the private function will continue to refer to the private implementation and the pattern doesn't apply to public members, only to functions.
+
+Public object members which refer to private variables are also subject to the no-patch rule notes above.
+
+As a result of this, modules created with the Revealing Module pattern may be more fragile than those created with the original Module pattern, so care should be taken during usage.
+
+*/
+
+var car = (function() {
+  var privateTotalMile = 0,
+      publicLastMile = 0;
+
+  var privateMethod = function() {
+    console.log('private method');
+  }
+
+  var publicShowLastMile = function() {
+    console.log(publicLastMile);
+  }
+  var publicShowMile = function() {
+    console.log(privateTotalMile);
+  };
+
+  var publicDrive = function(mile) {
+    publicLastMile = mile;
+    privateTotalMile += mile;
+    console.log(publicLastMile);
+  };
 
 
-// Example 1
+  // Reveal public pointers to
+  // private functions and properties
 
-var basket = (function() {
-  // private varialbe
-  var list = [],
-      total = 0;
-
-  // public methods
   return {
-    addItem: function(item) {
-      list.push(item);
-    },
-    totalPrice: function() {
-      list.forEach(function(obj){
-        console.log(obj);
-        total = total + obj.price * obj.amount;
-      });
-      return total;
-    },
-    showList: function() {
-      return list;
-    }
+    lastDrive: publicShowLastMile,
+    showTotal: publicShowMile,
+    drive:  publicDrive
   };
 
 })();
 
+car.drive(100);
+car.drive(50);
+var b = car.lastDrive();
 
-basket.addItem({
-  name: 'apple',
-  amount: 1,
-  price: 10
-});
-
-basket.addItem({
-  name: 'kiwi',
-  amount: 3,
-  price: 20
-});
-
-console.log(basket.showList());
-console.log(basket.totalPrice());
+car.showTotal();
