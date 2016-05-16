e55854fea353aef00b29d9f70332a46c6ed40beb
Dkra <dkra4741@gmail.com>
2016-05-17 01:56:29 +0800

    add browserify

--- become-better.js
+++ become-better.js
@@ -1,3 +1,147 @@
-let a = `roger`;
+"use strict";
 
-console.log(`Hi ${a}`);
+var _chart = require("src/chart.js");
+
+var _chart2 = _interopRequireDefault(_chart);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// import Chart from 'src/chart.js'
+// var Chart = require('src/chart.js')
+/*!
+ * Chart.js
+ * http://chartjs.org
+ *
+ * Copyright 2013 Nick Downie
+ * Released under the MIT license
+ * https://github.com/nnnick/Chart.js/blob/master/LICENSE.md
+ */
+
+// ======================================================
+// Radar Chart
+// ======================================================
+
+// Radar Chart Options
+var radarOptions = {
+
+	//Boolean - If we show the scale above the chart data
+	scaleOverlay: false,
+
+	//Boolean - If we want to override with a hard coded scale
+	scaleOverride: false,
+
+	//** Required if scaleOverride is true **
+	//Number - The number of steps in a hard coded scale
+	scaleSteps: null,
+	//Number - The value jump in the hard coded scale
+	scaleStepWidth: null,
+	//Number - The centre starting value
+	scaleStartValue: null,
+
+	//Boolean - Whether to show lines for each scale point
+	scaleShowLine: true,
+
+	//String - Colour of the scale line
+	scaleLineColor: "#999",
+
+	//Number - Pixel width of the scale line
+	scaleLineWidth: 1,
+
+	//Boolean - Whether to show labels on the scale
+	scaleShowLabels: false,
+
+	//Interpolated JS string - can access value
+	scaleLabel: "<%=value%>",
+
+	//String - Scale label font declaration for the scale label
+	scaleFontFamily: "'Arial'",
+
+	//Number - Scale label font size in pixels
+	scaleFontSize: 12,
+
+	//String - Scale label font weight style
+	scaleFontStyle: "normal",
+
+	//String - Scale label font colour
+	scaleFontColor: "#666",
+
+	//Boolean - Show a backdrop to the scale label
+	scaleShowLabelBackdrop: true,
+
+	//String - The colour of the label backdrop
+	scaleBackdropColor: "rgba(255,255,255,0.75)",
+
+	//Number - The backdrop padding above & below the label in pixels
+	scaleBackdropPaddingY: 2,
+
+	//Number - The backdrop padding to the side of the label in pixels
+	scaleBackdropPaddingX: 2,
+
+	//Boolean - Whether we show the angle lines out of the radar
+	angleShowLineOut: true,
+
+	//String - Colour of the angle line
+	angleLineColor: "rgba(255,255,255,0.3)",
+
+	//Number - Pixel width of the angle line
+	angleLineWidth: 1,
+
+	//String - Point label font declaration
+	pointLabelFontFamily: "'Arial'",
+
+	//String - Point label font weight
+	pointLabelFontStyle: "normal",
+
+	//Number - Point label font size in pixels
+	pointLabelFontSize: 12,
+
+	//String - Point label font colour
+	pointLabelFontColor: "#EFEFEF",
+
+	//Boolean - Whether to show a dot for each point
+	pointDot: true,
+
+	//Number - Radius of each point dot in pixels
+	pointDotRadius: 3,
+
+	//Number - Pixel width of point dot stroke
+	pointDotStrokeWidth: 1,
+
+	//Boolean - Whether to show a stroke for datasets
+	datasetStroke: true,
+
+	//Number - Pixel width of dataset stroke
+	datasetStrokeWidth: 1,
+
+	//Boolean - Whether to fill the dataset with a colour
+	datasetFill: true,
+
+	//Boolean - Whether to animate the chart
+	animation: true,
+
+	//Number - Number of animation steps
+	animationSteps: 60,
+
+	//String - Animation easing effect
+	animationEasing: "easeOutQuart",
+
+	//Function - Fires when the animation is complete
+	onAnimationComplete: null
+
+};
+
+// Radar Data
+var radarData = {
+	labels: ["游戏画面", "游戏xx", "游戏aa", "游戏bb", "游戏dd", "游戏ee", "游戏ff"],
+	datasets: [{
+		fillColor: "rgba(220,220,220,0.5)",
+		strokeColor: "rgba(220,220,220,1)",
+		data: [65, 59, 90, 81, 56, 55, 40]
+	}]
+};
+
+//Get the context of the Radar Chart canvas element we want to select
+var ctx = document.getElementById("radarChart").getContext("2d");
+
+// Create the Radar Chart
+var myRadarChart = new _chart2.default(ctx).Radar(radarData, radarOptions);
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var $ = require('gulp-load-plugins')();
 var cssnano = require('cssnano');
 var del = require('del');
+var source = require('vinyl-source-stream');
 var browserSync = require('browser-sync').create();
-
+var browserify = require('browserify');
+var babelify = require('babelify');
 /*****************************
   * Tasks
 
@@ -24,14 +27,14 @@ var path = {
   src: {
     jade: 'src/jade/**/*.jade',
     sass: './sass/*.sass',
-    scripts: ['src/js/**/*.js'],
+    scripts: ['js/*.js'],
     images: 'src/images/**/*',
   },
-  desc: {
+  dest: {
     path: 'build/',
-    html: 'build/**/*.html',
+    html: './*.html',
     css: './',
-    js: 'become-better.js'
+    js: './'
   }
 };
 
@@ -40,7 +43,7 @@ var path = {
 -----------------------------*/
 
 gulp.task('clean', function() {
-  return del([path.desc.path]);
+  return del([path.dest.path]);
 });
 
 /*-----------------------------
@@ -54,7 +57,7 @@ gulp.task('jade', function() {
 
   return gulp.src(path.src.jade)
     .pipe($.jade(config))
-    .pipe(gulp.dest(path.desc.path));
+    .pipe(gulp.dest(path.dest.path));
 });
 
 /*-----------------------------
@@ -66,7 +69,7 @@ gulp.task('sass', function() {
     .pipe($.sourcemaps.init())
     .pipe($.sass().on('error', $.sass.logError))
     .pipe($.sourcemaps.write('.'))
-    .pipe(gulp.dest(path.desc.css));
+    .pipe(gulp.dest(path.dest.css));
 });
 
 /*-----------------------------
@@ -78,13 +81,13 @@ gulp.task('css', function() {
     cssnano()
   ];
 
-  return gulp.src(path.desc.css)
+  return gulp.src(path.dest.css)
     // .pipe($.sourcemaps.init({loadMaps: true}))
     .pipe($.autoprefixer({browsers: ['last 2 version']}))
     .pipe($.postcss(processors))
     // .pipe($.sourcemaps.write())
     // .pipe($.uglify())
-    .pipe(gulp.dest(path.desc.css));
+    .pipe(gulp.dest(path.dest.css));
 });
 
 /*-----------------------------
@@ -92,15 +95,27 @@ gulp.task('css', function() {
 -----------------------------*/
 
 gulp.task('js', function() {
-  return gulp.src(path.scripts)
-    .pipe($.sourcemaps.init())
-    .pipe($.babel({
-      presets: ['es2015']
-    }))
-    // .pipe($.uglify())
-    // .pipe($.concat('main.js'))
-    .pipe($.sourcemaps.write('.'))
-    .pipe(gulp.dest(path.desc.js));
+  return browserify({
+                entries: ['./js/become-better.js'],
+                // transform: [babelify],
+                // extension: ['jsx', 'js', 'es6', ''],
+                debug: true
+            })
+            .transform(["babelify",{presets: ["es2015"]}])
+            .bundle()
+            .on('error', function(e) {
+              gutil.log(e);
+            })
+            .pipe(source('bundle.js'))
+            // .pipe(buffer())
+            .pipe($.sourcemaps.init({loadMaps: true}))
+            .pipe($.sourcemaps.write('./maps'))
+            // .pipe($.using({
+            //   prefix: 'bundle:js',
+            //   color: 'yellow'
+            // }))
+            .pipe(gulp.dest('./'))
+            .pipe(browserSync.stream());
 });
 
 /*-----------------------------
@@ -110,7 +125,7 @@ gulp.task('js', function() {
 gulp.task('images', ['clean'], function() {
   return gulp.src(path.src.images)
     .pipe($.imagemin({optimizationLevel: 5}))
-    .pipe(gulp.dest(path.desc.images));
+    .pipe(gulp.dest(path.dest.images));
 });
 
 /*-----------------------------
@@ -126,7 +141,7 @@ gulp.task('build', ['clean', 'jade', 'sass', 'css', 'js', 'images']);
 gulp.task('watch', function() {
   // gulp.watch(path.jade, ['jade']);
   gulp.watch(path.src.sass, ['sass']);
-  gulp.watch(path.desc.css, ['css']);
+  gulp.watch(path.dest.css, ['css']);
   gulp.watch(path.src.scripts, ['js']);
   // gulp.watch(path.images, ['images']);
 });
@@ -137,7 +152,7 @@ gulp.task('watch', function() {
 
 gulp.task('sync', ['watch'],function() {
   // Reload List
-  const reloadList = ['./*.css', path.desc.html, path.desc.js];
+  const reloadList = ['./*.css', path.dest.html, path.dest.js];
 
   browserSync.init({
     server: {
