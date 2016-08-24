01e5741d8d40aa7c165ee3fae0907d1af3d4e702
roger.jiang <dkra4741@gmail.com>
2016-08-24 15:13:10 +0800

    provide a easier usage interface

--- app/components/become-better.js
+++ app/components/become-better.js
@@ -1,7 +1,39 @@
 export default function() {
   /*
-  *   Become Better from here
+  *   Facade Pattern
+  *
+  *   Wiki: https://en.wikipedia.org/wiki/Facade_pattern
   */
+
+  var myevent = {
+    stop: function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+    }
+  };
+
+  // Handling browser differences (you don't want to do this all over your application, so put it in one facade
+  var myevent = {
+    stop: function(e) {
+      // Supported Browsers
+      if (typeof e.preventDefault === "function") {
+        e.preventDefault();
+      }
+
+      if (typeof e.stopPropagation === "function") {
+        e.stopPropagation();
+      }
+
+      // IE
+      if (typeof e.returnValue === "boolean") {
+        e.returnValue = false;
+      }
+
+      if (typeof e.cancalBubble === "boolean") {
+        e.cancalBubble = true;
+      }
+    }
+  };
 
 
 
