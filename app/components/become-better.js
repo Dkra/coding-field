e4e8e2543e02762f5c711b7fb55f77f8a0c757b1
roger.jiang <dkra4741@gmail.com>
2016-08-24 16:02:21 +0800

    use jquery GET, POST method for example

--- app/components/become-better.js
+++ app/components/become-better.js
@@ -3,38 +3,44 @@ export default function() {
   *   Facade Pattern
   *
   *   Wiki: https://en.wikipedia.org/wiki/Facade_pattern
+  *
+  *   Description:
+  *
+  *   Provide a unified interface to a set of interfaces in a subsystem.
+  *   Defines a high-level interface that makes the subsystem easier to use.
   */
 
-  var myevent = {
-    stop: function(e) {
-        e.preventDefault();
-        e.stopPropagation();
-    }
-  };
+  /*
+    Example from JQuery v3.1.0
 
-  // Handling browser differences (you don't want to do this all over your application, so put it in one facade
-  var myevent = {
-    stop: function(e) {
-      // Supported Browsers
-      if (typeof e.preventDefault === "function") {
-        e.preventDefault();
-      }
+    Because of the code as below so that we can easily use:
 
-      if (typeof e.stopPropagation === "function") {
-        e.stopPropagation();
-      }
+    jQuery.get( url, data, callback, type )
+    jQuery.post( url, data, callback, type )
 
-      // IE
-      if (typeof e.returnValue === "boolean") {
-        e.returnValue = false;
-      }
+    ** The code below is not executable **
+  */
 
-      if (typeof e.cancalBubble === "boolean") {
-        e.cancalBubble = true;
-      }
-    }
-  };
+  jQuery.each( [ "get", "post" ], function( i, method ) {
+  	jQuery[ method ] = function( url, data, callback, type ) {
 
+  		// Shift arguments if data argument was omitted
+  		if ( jQuery.isFunction( data ) ) {
+  			type = type || callback;
+  			callback = data;
+  			data = undefined;
+  		}
+
+  		// The url can be an options object (which then must have .url)
+  		return jQuery.ajax( jQuery.extend( {
+  			url: url,
+  			type: method,
+  			dataType: type,
+  			data: data,
+  			success: callback
+  		}, jQuery.isPlainObject( url ) && url ) );
+  	};
+  }
 
 
 
