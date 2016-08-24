a80144d6b45da7dc986af4f2671705b1cdf7776e
roger.jiang <dkra4741@gmail.com>
2016-08-24 21:57:01 +0800

    add comment diff between facade & adapter

--- app/components/become-better.js
+++ app/components/become-better.js
@@ -9,6 +9,11 @@ export default function() {
   *   The primary function of an Adapter is to produce a unified interface for a number of underlying and unrelated objects.
   *
   *   e.g: Ruby ORM - a unified interface as part of its API but the code underneath the interface is able to communicate with many different types of databases
+  *
+  *
+  *   The difference between a Facade and an Adapter is that the Facade makes a simple abstraction,
+  *   where as an Adapter will handle complex interactions by taking incoming data and constructing
+  *   it to work with the underlying objects.
   */
 
   var myevent = {
