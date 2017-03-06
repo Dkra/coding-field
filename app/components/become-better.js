b330c1b98948f997bc681aa23d324134a3f100d3
roger.jiang <roger.jiang@afusion.com>
2017-03-06 12:16:12 +0800

    add test folder

--- app/components/become-better.js
+++ app/components/become-better.js
@@ -1,9 +1,20 @@
 import { expect } from 'chai'
+import Im from 'immutable'
 
 export default function() {
   /*
   *   Become Better from here
   */
+
+  // fromJS()
+
+  // var map = new Map();
+  // Immutable.fromJS({a: {b: [10, 20, 30]}, c: 40}
+  // map.get("1"); // "one"
+  // map.get(1);   // undefined
+
+  
+
 
 
 
--- test/test.js
+++ test/test.js
@@ -1,8 +1,10 @@
-var assert = require('assert');
+import assert from 'assert'
+import { expect } from 'chai';
+
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
+      expect([1,2,3].indexOf()).to.be.a('number');
     });
   });
 });
